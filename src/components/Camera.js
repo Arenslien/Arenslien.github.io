ec0417a450f0e9dd030b5d5f7f36918aa1b58a59
Arenslien <arenslien@gmail.com>
2026-01-07 17:17:45 +0900

    Grass, Map 추가

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -2,10 +2,10 @@ import * as THREE from "three";
 
 export function Camera() {
   const size = 300;
-  const viewRatio = window.innerHeight / window.innerHeight;
+  const viewRatio = window.innerWidth / window.innerHeight;
   const width = viewRatio < 1 ? size : size * viewRatio;
   const height = viewRatio < 1 ? size / viewRatio : size;
-  
+
   const camera = new THREE.OrthographicCamera(
     width / -2, // left
     width / 2, // right
